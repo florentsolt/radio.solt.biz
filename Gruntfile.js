bb6ff19b02d717b7d5feb946b3d1b3a4ac17e93e
Florent Solt <florent@solt.biz>
2015-02-19 16:18:04 +0100

    Version bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,11 @@ module.exports = function(grunt) {
     nodewebkit: {
       options: {
         name: "Radio.Solt.Biz",
-        version: '0.11.0',
+        version: '0.11.6',
         build_dir: './build',
         mac_icns: './icon/icon.icns',
         winIco: './icon/icon.ico',
-        mac: true,
-        win: true,
-        linux32: false,
-        linux64: false
+        platforms: ['osx32', 'win32']
       },
       src: [
       './node_modules/**',
@@ -22,21 +19,23 @@ module.exports = function(grunt) {
       ]
     },
     clean: {
-      main: ['build/*'],
+      build: ['build/*'],
       cache: ['cache/*']
     },
     copy: {
       main: {
         files: [
         {
-          src: 'libraries/win/ffmpegsumo.dll',
-          dest: 'build/Radio.Solt.Biz/win/ffmpegsumo.dll',
-          flatten: true
+          src: 'libraries/win32/ffmpegsumo.dll',
+          dest: 'build/Radio.Solt.Biz/win32/ffmpegsumo.dll'
         },
         {
-          src: 'libraries/osx/ffmpegsumo.so',
-          dest: 'build/Radio.Solt.Biz/osx/Radio.Solt.Biz.app/Contents/Frameworks/node-webkit Framework.framework/Libraries/ffmpegsumo.so',
-          flatten: true
+          src: 'libraries/osx32/ffmpegsumo.so',
+          dest: 'build/Radio.Solt.Biz/osx32/Radio.Solt.Biz.app/Contents/Frameworks/node-webkit Framework.framework/Libraries/ffmpegsumo.so'
+        },
+        {
+          src: 'libraries/osx64/ffmpegsumo.so',
+          dest: 'build/Radio.Solt.Biz/osx64/Radio.Solt.Biz.app/Contents/Frameworks/node-webkit Framework.framework/Libraries/ffmpegsumo.so'
         }
         ]
       }
@@ -65,6 +64,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-node-webkit-builder');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.registerTask('default', ['nodewebkit', 'copy']);
+  grunt.registerTask('default', ['clean:build', 'nodewebkit', 'copy']);
 
 };
